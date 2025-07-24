f34d5f6368a0685d7fe518eb611ae84e937485ad
Emman <emtsobrevega@gmail.com>
2025-07-25 03:47:25 +0800

    feat(members): enhance members list with avatar, update and delete functionalities

--- src/app/(standalone)/workspaces/[workspaceId]/members/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/members/page.tsx
@@ -1,16 +1,15 @@
 import { getCurrent } from "@/features/auth/queries";
+import { getMember } from "@/features/members/utils";
 import { MembersList } from "@/features/workspaces/components/members-list";
 
 import { redirect } from "next/navigation";
 
-const WorkspaceIdMembersPage = async () => {
+const WorkspaceIdMembersPage = async ({}) => {
     const user = await getCurrent();
     if (!user) redirect("/sign-in");
 
-
     return (
         <div className="w-full lg:max-w-xl">
-            Workspace ID Members Page 
             <MembersList />
         </div>
     );
